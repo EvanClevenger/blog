650e3b954ae160154eeb3e58744c3acba2a0f20a
Evan Clevenger <evanclevenger@gmail.com>
2023-01-29 18:34:43 -0600

    added to tailwind css components to home page

--- pages/index.js
+++ pages/index.js
@@ -4,25 +4,33 @@ import Head from 'next/head'
 const posts = [ 
   { title: 'React Testing', excerpt: 'Learning React Testing'},
   { title: 'React with Tailwind', excerpt: 'Learning React With Tailwind'},
+  { title: 'React with Tailwind', excerpt: 'Learning React With Tailwind'}
 ];
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-10 mb-8 bg-grey-300">
+    <div className="container mx-auto px-10 mb-8">
       <Head>
         <title>Game Haus</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        
+        <div classname='lg:col0span-8 col-span-1'>
         {posts.map((post, index) => (
           <div>
             {post.title}
             {post.excerpt}
           </div>  
         ))}
+    </div>
+      <div className='lg:col-span-4 col-span-1'>
+        <div className='lg:sticky relative top-8'>
+
+        </div>
+
       </div>
+    </div>
 
     </div>
   )
