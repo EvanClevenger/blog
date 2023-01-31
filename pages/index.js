dd8cb7b4faeabdaf36d90c4a933b56bd182432c3
Evan Clevenger <evanclevenger@gmail.com>
2023-01-31 15:33:03 -0600

    added ES7 React snippet from extentions

--- pages/index.js
+++ pages/index.js
@@ -15,17 +15,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        <div classname='lg:col0span-8 col-span-1'>
+    <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+      <div classname='lg:col-span-8 col-span-1'>
         {posts.map((post, index) => (
           <div>
             {post.title}
             {post.excerpt}
           </div>  
         ))}
-    </div>
+      </div>
       <div className='lg:col-span-4 col-span-1'>
         <div className='lg:sticky relative top-8'>
+          
 
         </div>
 
