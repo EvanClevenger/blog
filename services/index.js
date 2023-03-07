cc80151ada09ba9884caf59ba3b2f7bdcd2c5286
Evan Clevenger <evanclevenger@gmail.com>
2023-03-06 18:28:05 -0600

    added query with graphcms

--- services/index.js
+++ services/index.js
@@ -1,1 +1,32 @@
 import { request, gql } from 'graphql-request' ;
+
+export const getPosts = async () => {
+    const query = gql `
+    query MyQuery {
+        postsConnection {
+          edges {
+            node {
+              author {
+                name
+                id
+                photo {
+                  url
+                }
+              }
+              createdAt
+              slug
+              title
+              exerpt
+              featuredImage {
+                url
+              }
+              categories {
+                name
+                slug
+              }
+            }
+          }
+        }
+      }
+    `
+}
